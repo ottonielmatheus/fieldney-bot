7e4e3ccffdfb21a5dbb93466715053b840c61e1e
Ottoniel Matheus <ottoniel.matheus@gmail.com>
2023-07-30 01:45:53 -0300

    ci: 🎡 change github handler path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { createLambdaFunction, createProbot } = require('@probot/adapter-aws-lambda-serverless')
 const { awsLambdaReceiver } = require('./src/core/integrations/slack')
-const appFn = require('./src/app')
+const appFn = require('./src/github/app')
 
 module.exports = {
   github: createLambdaFunction(appFn, { probot: createProbot() }),
